33003708182b776912307363e6444751985a381b
s4kr4 <s4kr4m4@gmail.com>
2017-07-21 00:17:52 +0900

    Add login test case and fix bugs

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:permanent_token] = user.remember_token
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def forget(user)
@@ -24,7 +24,7 @@ module SessionsHelper
 
   def current_user
     if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
