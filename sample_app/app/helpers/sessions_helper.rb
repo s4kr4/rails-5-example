725242b83dbdfe2b3c52df3de1c213663c29d5a6
s4kr4 <s4kr4m4@gmail.com>
2017-10-04 01:19:41 +0900

    Protect from wrong users, implement frendly forwarding

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -22,6 +22,10 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -36,5 +40,14 @@ module SessionsHelper
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
