6c829ae5a5cfc4ecaefbf4acefe68d0ffca724c0
s4kr4 <s4kr4m4@gmail.com>
2017-11-01 03:57:41 +0000

    Enable react

--- sample_app/app/javascript/packs/application.js
+++ sample_app/app/javascript/packs/application.js
@@ -7,4 +7,13 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render((
+    <div className="root">
+      root
+    </div>
+  ), document.getElementById('root'))
+})
